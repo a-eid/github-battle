3daf8ac7d2986bb09da6506ead33941d27b59175
Ahmed Eid <a.eid@yandex.com>
2017-04-20 03:11:44 +0300

    add fix for really long rep names

--- src/components/popular/repo-grid-item.js
+++ src/components/popular/repo-grid-item.js
@@ -6,7 +6,7 @@ const RepoGridItem = ({repo , rank}) => (
     <ul className="item">
       <li># {rank}</li> 
       <li><img className="avatar" src={repo.owner.avatar_url} alt="" /></li> 
-      <li><a href={repo.html_url}>{repo.name}</a> </li> 
+      <li><a href={repo.html_url}>{repo.name.length > 20 ? repo.name.substr(0 , 17) + "..." : repo.name }</a> </li> 
       <li><a href={repo.owner.html_url} >{repo.owner.login}</a></li> 
       <li>{repo.stargazers_count} stars{console.log(repo)}</li> 
     </ul>  
