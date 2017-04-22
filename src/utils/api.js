1896fcb0e5405d3f6a2622dac8254675f507c7b2
Ahmed Eid <a.eid@yandex.com>
2017-04-22 15:51:38 +0300

    api fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default {
   fetchPopularRepos: (language) => {
-    const uri = encodeURI(`https://api.github.com/search/repositories?q=stars:>100+${language}:javascript&sort=stars&order=desc&type=repositories`)
+    const uri = encodeURI(`https://api.github.com/search/repositories?q=stars:>100+language:${language}&sort=stars&order=desc&type=repositories`)
   return axios(uri).then(res => res.data.items )
   }
 }
